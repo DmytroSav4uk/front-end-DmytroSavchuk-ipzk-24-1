c4b84258fe312ea5df5713f0b4ac7270b97cd83f
Dmytro Savchuk <dmytro.savchuk.ua@gmail.com>
2024-10-23 16:55:36 +0300

    l7 changes

--- router.js
+++ router.js
@@ -166,7 +166,7 @@ function task2() {
     function getRandomColor() {
         const letters = '0123456789ABCDEF';
         let color = '#';
-        for (let i = 0; i < 6; i++) {
+        for (let i = 0; i < 3; i++) {
             color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
